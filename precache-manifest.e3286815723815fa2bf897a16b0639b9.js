718feed595f63c3b564c305475ba8d4fbdb2a50d
nicole <nicole@cgitcayman.com>
2019-04-17 15:43:29 +0800

    Updates

---
+++ precache-manifest.e3286815723815fa2bf897a16b0639b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJsShopCart/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a54a5975e88de6da9a2d",
+    "url": "/ReactJsShopCart/static/js/runtime~main.830e0b2b.js"
+  },
+  {
+    "revision": "ab8f3b8c58228c63147e",
+    "url": "/ReactJsShopCart/static/js/main.54f0252e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ReactJsShopCart/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ab8f3b8c58228c63147e",
+    "url": "/ReactJsShopCart/static/css/main.29a2da57.chunk.css"
+  },
+  {
+    "revision": "4e6194d35d018745e9955ccb81d98378",
+    "url": "/ReactJsShopCart/index.html"
+  }
+];
