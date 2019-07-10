f149e71c6e4acf16f8137babb22c9a9656734c8c
nicole <nicole@cgitcayman.com>
2019-07-10 11:00:05 +0800

    Category 改為一般變數非 state

--- shop-cart/src/hoc/Layout/Layout.js
+++ shop-cart/src/hoc/Layout/Layout.js
@@ -8,129 +8,10 @@ import List from '../../containers/List/List';
 import SmallDeviceWrapper from '../../components/SmallDeviceWrapper/SmallDeviceWrapper';
 import axios from '../../axios-orders';
 
+let dbCategory = null;
+
 class Layout extends Component {
-
-  /*     componentDidMount(){
-        const category = {
-          category: [
-            {
-              category: 1,
-              name: "Category1",
-              item: [
-                {
-                  id: 1,
-                  name: 'item1-1'
-                },
-                {
-                  id: 2,
-                  name: 'item1-2'
-                },
-                {
-                  id: 3,
-                  name: 'item1-3'
-                }
-              ]
-            },
-            {
-              category: 2,
-              name: "Category2",
-              item: [
-                {
-                  id: 1,
-                  name: 'item2-1'
-                },
-                {
-                  id: 2,
-                  name: 'item2-2'
-                },
-                {
-                  id: 3,
-                  name: 'item2-3'
-                }
-              ]
-            },
-            {
-              category: 3,
-              name: "Category3",
-              item: [
-                {
-                  id: 1,
-                  name: 'item3-1'
-                },
-                {
-                  id: 2,
-                  name: 'item3-2'
-                },
-                {
-                  id: 3,
-                  name: 'item3-3'
-                }
-              ]
-            },
-            {
-              category: 4,
-              name: "Category4",
-              item: [
-                {
-                  id: 1,
-                  name: 'item4-1'
-                },
-                {
-                  id: 2,
-                  name: 'item4-2'
-                },
-                {
-                  id: 3,
-                  name: 'item4-3'
-                }
-              ]
-            },
-            {
-              category: 5,
-              name: "Category5",
-              item: [
-                {
-                  id: 1,
-                  name: 'item5-1'
-                },
-                {
-                  id: 2,
-                  name: 'item5-2'
-                },
-                {
-                  id: 3,
-                  name: 'item5-3'
-                }
-              ]
-            },
-            {
-              category: 6,
-              name: "Category6",
-              item: [
-                {
-                  id: 1,
-                  name: 'item6-1'
-                },
-                {
-                  id: 2,
-                  name: 'item6-2'
-                },
-                {
-                  id: 3,
-                  name: 'item6-3'
-                }
-              ]
-            }
-          ]
-        };
-  
-        axios.post('category.json', category)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    } */
-
   state = {
-    category: null,
     searchKeyword: '',
     product: null,
     productForRender: null
@@ -140,7 +21,7 @@ class Layout extends Component {
     axios.get('category.json')
       .then(reponse => {
         //console.log("Category Get Data", reponse.data);
-        this.setState({ category: reponse.data });
+        dbCategory = reponse.data;
       })
       .catch(error => {
         console.log("Category Get error", error);
@@ -193,7 +74,7 @@ class Layout extends Component {
   }
 
   render() {
-    const categoryData = { ...this.state.category };
+    const categoryData = { ...dbCategory };
 
     let category = [];
     Object.keys(categoryData)
